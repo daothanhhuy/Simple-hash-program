031e9dd8f70f3dbcfd1f5a960c302088024d8663
daothanhhuy <19521601@gm.uit.edu.vn>
2021-11-29 14:36:23 +0700

    Fist commit

---
+++ HashProgram/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace HashProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SelectInputCB = new System.Windows.Forms.ComboBox();
+            this.InputTB = new System.Windows.Forms.TextBox();
+            this.md5OutputTb = new System.Windows.Forms.TextBox();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.md5Cb = new System.Windows.Forms.CheckBox();
+            this.sha1Cb = new System.Windows.Forms.CheckBox();
+            this.sha1OutputTb = new System.Windows.Forms.TextBox();
+            this.sha2Cb = new System.Windows.Forms.CheckBox();
+            this.sha2OutputTb = new System.Windows.Forms.TextBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SelectInputCB
+            // 
+            this.SelectInputCB.FormattingEnabled = true;
+            this.SelectInputCB.Items.AddRange(new object[] {
+            "Text String",
+            "Hex String",
+            "Binary File"});
+            this.SelectInputCB.Location = new System.Drawing.Point(12, 12);
+            this.SelectInputCB.Name = "SelectInputCB";
+            this.SelectInputCB.Size = new System.Drawing.Size(79, 23);
+            this.SelectInputCB.TabIndex = 0;
+            this.SelectInputCB.SelectedIndexChanged += new System.EventHandler(this.SelectInputCB_SelectedIndexChanged);
+            // 
+            // InputTB
+            // 
+            this.InputTB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTB.Location = new System.Drawing.Point(97, 12);
+            this.InputTB.Name = "InputTB";
+            this.InputTB.Size = new System.Drawing.Size(876, 23);
+            this.InputTB.TabIndex = 2;
+            this.InputTB.TextChanged += new System.EventHandler(this.InputTB_TextChanged);
+            // 
+            // md5OutputTb
+            // 
+            this.md5OutputTb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.md5OutputTb.Location = new System.Drawing.Point(97, 65);
+            this.md5OutputTb.Name = "md5OutputTb";
+            this.md5OutputTb.Size = new System.Drawing.Size(876, 23);
+            this.md5OutputTb.TabIndex = 3;
+            this.md5OutputTb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(899, 186);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertBtn.TabIndex = 4;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // md5Cb
+            // 
+            this.md5Cb.AutoSize = true;
+            this.md5Cb.Location = new System.Drawing.Point(20, 69);
+            this.md5Cb.Name = "md5Cb";
+            this.md5Cb.Size = new System.Drawing.Size(56, 19);
+            this.md5Cb.TabIndex = 16;
+            this.md5Cb.Text = "MD-5";
+            this.md5Cb.UseVisualStyleBackColor = true;
+            // 
+            // sha1Cb
+            // 
+            this.sha1Cb.AutoSize = true;
+            this.sha1Cb.Location = new System.Drawing.Point(20, 110);
+            this.sha1Cb.Name = "sha1Cb";
+            this.sha1Cb.Size = new System.Drawing.Size(60, 19);
+            this.sha1Cb.TabIndex = 17;
+            this.sha1Cb.Text = "SHA-1";
+            this.sha1Cb.UseVisualStyleBackColor = true;
+            // 
+            // sha1OutputTb
+            // 
+            this.sha1OutputTb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sha1OutputTb.Location = new System.Drawing.Point(97, 106);
+            this.sha1OutputTb.Name = "sha1OutputTb";
+            this.sha1OutputTb.Size = new System.Drawing.Size(876, 23);
+            this.sha1OutputTb.TabIndex = 18;
+            // 
+            // sha2Cb
+            // 
+            this.sha2Cb.AutoSize = true;
+            this.sha2Cb.Location = new System.Drawing.Point(20, 151);
+            this.sha2Cb.Name = "sha2Cb";
+            this.sha2Cb.Size = new System.Drawing.Size(72, 19);
+            this.sha2Cb.TabIndex = 19;
+            this.sha2Cb.Text = "SHA-256";
+            this.sha2Cb.UseVisualStyleBackColor = true;
+            // 
+            // sha2OutputTb
+            // 
+            this.sha2OutputTb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sha2OutputTb.Location = new System.Drawing.Point(98, 147);
+            this.sha2OutputTb.Name = "sha2OutputTb";
+            this.sha2OutputTb.Size = new System.Drawing.Size(876, 23);
+            this.sha2OutputTb.TabIndex = 20;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(818, 186);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 21;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 224);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.sha2OutputTb);
+            this.Controls.Add(this.sha2Cb);
+            this.Controls.Add(this.sha1OutputTb);
+            this.Controls.Add(this.sha1Cb);
+            this.Controls.Add(this.md5Cb);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.md5OutputTb);
+            this.Controls.Add(this.InputTB);
+            this.Controls.Add(this.SelectInputCB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox SelectInputCB;
+        private System.Windows.Forms.TextBox InputTB;
+        private System.Windows.Forms.TextBox md5OutputTb;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textToStoreTb;
+        private System.Windows.Forms.TextBox binaryMd5Tb;
+        private System.Windows.Forms.CheckBox md5Cb;
+        private System.Windows.Forms.CheckBox sha1Cb;
+        private System.Windows.Forms.TextBox sha1OutputTb;
+        private System.Windows.Forms.CheckBox sha2Cb;
+        private System.Windows.Forms.TextBox sha2OutputTb;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ HashProgram/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace HashProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SelectInputCB.SelectedIndex = 1; 
+            setOpenFileDialog();
+            
+        }
+
+        int choice = 0;
+
+        private void SelectInputCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            choice = cb.SelectedIndex;
+            switch (choice)
+            {
+                case 0:
+                    InputTB.ReadOnly = false;
+                    InputTB.Text = "";
+                    break;
+                case 1:
+                    InputTB.ReadOnly = false;
+                    InputTB.Text = "";
+                    break;
+                case 2:
+                    binfilePath = openBinFile();
+                    InputTB.ReadOnly = true;
+                    break;
+
+            }
+        }
+
+        private static string binfilePath = "";
+        private string openBinFile()
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                InputTB.Text = ofd.FileName;
+                return ofd.FileName;
+            } else
+            {
+                return "";
+            }
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            if (isCheck())
+            { 
+                switch (choice)
+                {
+                    case 0:
+                        byte[] inputBytes = Encoding.ASCII.GetBytes(InputTB.Text.ToString());
+                        startHash(inputBytes);
+                        break;
+                    case 1:
+                        if (OnlyHexInString(InputTB.Text.ToString()))
+                        {
+                            byte[] inputByte = Encoding.ASCII.GetBytes(hex2string(InputTB.Text.ToString()));
+                            startHash(inputByte);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Please enter correct HEX format.");
+                        }
+                        break;
+                   case 2:
+                        if (binfilePath != "")
+                        {
+                            byte[] input = File.ReadAllBytes(binfilePath);
+                            startHash(input);
+                        } else
+                        {
+                            MessageBox.Show("Plase select a file.");
+                        }
+                
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please choice at least 1 option.");
+/*
+                md5OutputTb.Text = "";
+                sha1OutputTb.Text = "";
+                sha1OutputTb.Text = "";
+*/
+                return;
+            }
+
+        }
+
+        private bool isCheck()
+        {
+            if (!md5Cb.Checked && !sha1Cb.Checked && !sha2Cb.Checked) return false;
+            return true;
+        }
+
+        private static bool OnlyHexInString(string test)
+        {
+            // For C-style hex notation (0xFF) you can use @"\A\b(0[xX])?[0-9a-fA-F]+\b\Z"
+            return System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0-9a-fA-F]+\b\Z");
+        }
+
+        private void startHash(byte[] input)
+        {
+
+            if (md5Cb.Checked)
+            {
+                md5OutputTb.Text = md5HashCalculate(input);
+            }
+
+            if (sha1Cb.Checked)
+            {
+                sha1OutputTb.Text = sha1HashCalculate(input);
+            }
+
+            if (sha2Cb.Checked)
+            {
+                sha2OutputTb.Text = sha2HashCalculate(input);
+            }
+        }
+
+        private void showValueBtn_Click(object sender, EventArgs e)
+        {
+            switch (choice)
+            {
+                case 0:
+                    MessageBox.Show(InputTB.Text.ToString());
+                    break;
+            }
+        }
+
+        private static String hex2string(String hex) 
+        {
+
+            string ascii = string.Empty;
+            for (int i = 0; i < hex.Length; i += 2) 
+            {
+                ascii += (char)HexadecimalToDecimal(hex.Substring(i, 2));
+
+            }
+            return ascii;
+        }
+
+        private static int HexadecimalToDecimal(string hex)
+        {
+            hex = hex.ToUpper();
+
+            int hexLength = hex.Length;
+            double dec = 0;
+
+            for (int i = 0; i < hexLength; ++i)
+            {
+                byte b = (byte)hex[i];
+
+                if (b >= 48 && b <= 57)
+                    b -= 48;
+                else if (b >= 65 && b <= 70)
+                    b -= 55;
+
+                dec += b * Math.Pow(16, ((hexLength - i) - 1));
+            }
+
+            return (int)dec;
+        }
+
+        private static String md5HashCalculate(byte[] inputBytes)
+        {
+            using (MD5 md5 = MD5.Create()) //  Creates an instance of the default implementation of the MD5 hash algorithm.
+            {
+                byte[] hahsBytes = md5.ComputeHash(inputBytes); // Computes the hash value for the specified byte array.
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < hahsBytes.Length; i++)
+                {
+                    sb.Append(hahsBytes[i].ToString("X2"));
+                }
+
+                return sb.ToString().ToLower(); 
+
+            }
+        }
+
+        private String sha1HashCalculate(byte [] inputBytes)
+        {
+            using(SHA1 sha1 = SHA1.Create())
+            {
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for(int  i= 0; i< hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString().ToLower();
+            }
+        }
+
+        private String sha2HashCalculate(byte[] inputBytes)
+        {
+            using (SHA256 sha2 = SHA256.Create())
+            {
+                byte[] hashBytes = sha2.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString().ToLower();
+            }
+        }
+
+        // Check hex string
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+
+        private static OpenFileDialog ofd = new OpenFileDialog();
+
+        private static void setOpenFileDialog()
+        {
+            ofd.InitialDirectory = @"C:\";
+            //ofd.DefaultExt = "txt";
+            ofd.RestoreDirectory = true;
+            // Cấu trúc filter như sau: <tên file> (<đuôi file>)|<*.đuôi file>
+            ofd.Filter = "txt files (*.txt)|*.txt|binary files (*.bin)|*bin|All files (*.*)|*.*";
+            
+            // ta có 3 cái filter, giả sử ta muốn mặc định mở ofd sẽ chọn filter thứ 3
+            ofd.FilterIndex = 3;
+            // Cho phép chọn nhiều file
+            //ofd.Multiselect = true;
+
+
+        }
+
+
+      
+        private static void createBinaryFile()
+        {
+            try
+            {
+                // file name
+                string fileName = @"D:\info.bin";
+
+                // Info
+                string authorName = "Thanh Huy";
+                int age = 20;
+                string[] favoriteFood = new string[] { "Pho", "Mi", "Hu tieu" };
+                bool isHandsome = true;
+                double height = 1.79;
+
+                /* BinaryWriter bwStream = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+                 Encoding ascii = Encoding.ASCII;*/
+
+                using (BinaryWriter binWriter = new BinaryWriter(new FileStream(fileName, FileMode.Create)))
+                {
+                    binWriter.Write(authorName);
+                    binWriter.Write(age);
+                    for (int i = 0; i < favoriteFood.Length; i++)
+                    {
+                        binWriter.Write(favoriteFood[i]);
+                    }
+                    binWriter.Write("Is handsome:" + isHandsome);
+                    binWriter.Write(height);
+                    MessageBox.Show("Write data successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Online MD5 tool
+        //https://emn178.github.io/online-tools/md5_checksum.html
+        //
+        private void openBinaryBtn_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                MD5 mD5 = MD5.Create();
+
+                FileStream fs = File.OpenRead(ofd.FileName);
+                byte[] output = File.ReadAllBytes(ofd.FileName);
+                byte[] outputBytes = mD5.ComputeHash(output);
+                StringBuilder sb = new StringBuilder();
+
+                for(int  i= 0;i < outputBytes.Length; i++)
+                {
+                    sb.Append(outputBytes[i].ToString("X2"));
+                }
+                binaryMd5Tb.Text = sb.ToString();
+/*
+                MD5 mD5 = MD5.Create();
+                byte[] inputBytes = File.ReadAllBytes(ofd.FileName);
+                byte[] outputBytes = mD5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for(int i=0;i < outputBytes.Length; i++)
+                {
+                    sb.Append(outputBytes[i].ToString("X2"));
+                }
+
+                binaryMd5Tb.Text = sb.ToString();
+*/
+            }
+        }
+
+        private void InputTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            InputTB.Text = "";
+            md5OutputTb.Text = "";
+            sha1OutputTb.Text = "";
+            sha2OutputTb.Text = "";
+        }
+    }
+}
